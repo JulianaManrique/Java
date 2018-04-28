e5217931f5fc2d907a7d3620cfc54f8fd2db2fed
JulianaManrique <36675613+JulianaManrique@users.noreply.github.com>
2018-04-27 19:24:52 -0500

    Add files via upload

---
+++ JAVA1/script2.js
@@ -0,0 +1,28 @@
+window.onload = function(suma) {
+    const denominador = div=> document.getElementById(div);
+    const calcula = () => ;
+
+
+
+
+    let numeradorUno =  nomDiv("numeradorUno").value;
+    let denominadorUno = nomDiv("denominadorUno").value;
+    let numeradorDos = nomDiv("numeradorDos").value;
+    let denominadorDos = nomDiv("denominadorDos").value;
+    if (numerador.length === 0, estatura.length === 0) {
+        alert("por favor complete los campos");
+    } else {
+        const denominador = denominadorUno * denominadorDos;
+        const numerodorOpera
+        const numeradorCorrecto = numerodorOperativo + numeradorUno;
+        
+    } 
+
+
+
+
+
+
+
+
+
